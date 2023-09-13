5cea1d3af44f5ce307d6df74bca0e1ad93b91172
MrKekovich <stas.kuryanov50@mail.ru>
2023-09-13 20:23:02 +0300

    Env setup

--- snake_game.py
+++ snake_game.py
@@ -1,5 +1,38 @@
+import os
+
+from dotenv import load_dotenv
+
 from app.game import Game
 
 if __name__ == '__main__':
-    game = Game()
+    # Load environment variables
+    load_dotenv()
+    try:
+        speed = int(os.getenv('SPEED', '10'))
+        food_amount = int(os.getenv('FOOD_AMOUNT', '3'))
+        random_initial_position = bool(os.getenv('RANDOM_INITIAL_POSITION', 'True'))
+        random_initial_direction = bool(os.getenv('RANDOM_INITIAL_DIRECTION', 'True'))
+        snake_body_color = tuple(map(int,
+                                     os.getenv('SNAKE_BODY_COLOR', '(0,255,0)').split(',')))
+        snake_head_color = tuple(map(int,
+                                     os.getenv('SNAKE_HEAD_COLOR', '(0,150,0)').split(',')))
+        background_color = tuple(os.getenv('BACKGROUND_COLOR', '(0,0,0)').split(','))
+        food_color = tuple(os.getenv('FOOD_COLOR', '(255,255,255)').split(','))
+        width = int(os.getenv('WIDTH', '30'))
+        height = int(os.getenv('HEIGHT', '40'))
+        game = Game(speed,
+                    food_amount,
+                    random_initial_position,
+                    random_initial_direction,
+                    snake_body_color,
+                    snake_head_color,
+                    background_color,
+                    food_color,
+                    width,
+                    height)
+    except ValueError as e:
+        print(f"Invalid environment variables:\n {e}")
+        print("Using default values")
+        game = Game()
+
     game.run()
